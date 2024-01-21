8bb6d15ce9c4c5d33dc38dea0cecdca335f3efe4
Gilles GROS <gilles@71yeti.fr>
2024-01-21 21:52:09 +0100

    update

--- src/settings/settings-manager.ts
+++ src/settings/settings-manager.ts
@@ -1,14 +1,17 @@
 import {Settings} from "./settings"
-import * as Cookie from "js-cookie";
+import Cookies from "js-cookie";
 
 export function loadSettings(): Settings {
   try {
-/*	with js-cookie 2.2.1 */
+/*	with js-cookie 2.2.1 
     const options = Cookie.getJSON("options") || {};
-/*	with js-cookie 3.0.2 
-	const options = JSON.parse(Cookie.get("options")) || {};
-	Cookie.getJSON("options") || {};
-*/
+    */
+/*	with js-cookie 3.0.2 */
+let options = Cookies.get("options");
+if (options) {
+  options = JSON.parse(options);
+}
+	//const options = JSON.parse(Cookie.get("options")) || {};
     return Settings.createFromObject(options);
   } catch (e) {
     console.error("Failed to load settings: ", e);
@@ -19,35 +22,35 @@ export function loadSettings(): Settings {
 export function saveSettings(settings: Settings) {
   if (location.hostname.indexOf("localhost") == -1) {
 /*	with js-cookie 2.2.1 */
-    Cookie.set("options", settings, {
+    Cookies.set("options", 'settings', {
       expires: 365, 
       sameSite: "none", 
       secure: true
     });
-/*	with js-cookie 3.0.2
-    Cookie.set("options", settings, 
+/*	with js-cookie 3.0.2 
+    Cookies.set("options", settings, 
 	  JSON.stringify({
         expires: 365, 
         sameSite: "none", 
         secure: true
       })
-	);
- */	
+	);*/
+
   } else {
 /*	with js-cookie 2.2.1 */
-    Cookie.set("options", settings, {
+    Cookies.set("options", 'settings', {
       expires: 365, 
       sameSite: "strict", 
       secure: false
-    });
-/*	with js-cookie 3.0.2
-    Cookie.set("options", settings, 
+    }); 
+/*	with js-cookie 3.0.2 
+    Cookies.set("options", settings, 
 	  JSON.stringify({
         expires: 365, 
         sameSite: "strict", 
         secure: false
       })
-	);
- */		
+	);*/
+ 
   }
 }
