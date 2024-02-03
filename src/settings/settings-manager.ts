3703be84eb3e3320f8659b0088c048eca765c225
Gilles GROS <gilles@71yeti.fr>
2024-02-03 12:10:08 +0100

    Final fix for js-cookie 3.0.5

--- src/settings/settings-manager.ts
+++ src/settings/settings-manager.ts
@@ -1,16 +1,25 @@
 import {Settings} from "./settings"
 import Cookies from "js-cookie";
+//import * as Cookies from "js-cookie";
 
 export function loadSettings(): Settings {
+
+  console.log("in loadSettings")
   try {
-/*	with js-cookie 2.2.1 
-    const options = Cookie.getJSON("options") || {};
-    */
+/*	with js-cookie 2.2.1  */
+   //const options = Cookies.getJSON("options") || {};
 /*	with js-cookie 3.0.2 */
-let options = Cookies.get("options");
-if (options) {
-  options = JSON.parse(options);
-}
+/* */
+    const readOptions = Cookies.get("options");
+    let options
+    if (typeof readOptions === "string" || typeof readOptions ==="undefined" ) {
+      options = {};
+    } else {
+      options = JSON.parse(readOptions);
+    }
+
+/* */
+  console.log(options)
 	//const options = JSON.parse(Cookie.get("options")) || {};
     return Settings.createFromObject(options);
   } catch (e) {
@@ -20,37 +29,26 @@ if (options) {
 }
 
 export function saveSettings(settings: Settings) {
+  console.log("in saveSettings")
   if (location.hostname.indexOf("localhost") == -1) {
-/*	with js-cookie 2.2.1 */
-    Cookies.set("options", 'settings', {
+    Cookies.set("options", 
+    /*	with js-cookie 2.2.1 */
+    // settings, {
+    /*	with js-cookie 3.0.2 */
+    JSON.stringify(settings), {
       expires: 365, 
-      sameSite: "none", 
+      sameSite: "none",
       secure: true
     });
-/*	with js-cookie 3.0.2 
-    Cookies.set("options", settings, 
-	  JSON.stringify({
-        expires: 365, 
-        sameSite: "none", 
-        secure: true
-      })
-	);*/
-
   } else {
-/*	with js-cookie 2.2.1 */
-    Cookies.set("options", 'settings', {
+  Cookies.set("options", 
+    /*	with js-cookie 2.2.1 */
+    // settings, {
+    /*	with js-cookie 3.0.2 */
+    JSON.stringify(settings), {
       expires: 365, 
-      sameSite: "strict", 
+      sameSite: "strict",
       secure: false
-    }); 
-/*	with js-cookie 3.0.2 
-    Cookies.set("options", settings, 
-	  JSON.stringify({
-        expires: 365, 
-        sameSite: "strict", 
-        secure: false
-      })
-	);*/
- 
+    });
   }
 }
